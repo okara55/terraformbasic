dfb384c6a5c663a2daa40829588c62a70a8aa71c
okara55 <57303507+okara55@users.noreply.github.com>
2022-03-17 00:07:05 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+    version = "2.4.0"
+ features {}
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+  tags = {
+    "owner" = "osman"
+  }
+}
+output "id" {
+    value = data.azurerm_resource_group.example.id
+  
+}
