818d479ffa0240651138a3cf524142882d682d11
okara55 <57303507+okara55@users.noreply.github.com>
2022-03-17 14:09:23 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,17 +3,11 @@ terraform {
 }
 
 provider "azurerm" {
-    version = "2.4.0"
+ version = "2.20.0"
  features {}
 }
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
-  tags = {
-    "owner" = "osman"
-  }
 }
-output "id" {
-    value = data.azurerm_resource_group.example.id
-  
-}
+
