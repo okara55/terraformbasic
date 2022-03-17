02ab5148c1d48950577487c16405f8c09b15eebd
okara55 <57303507+okara55@users.noreply.github.com>
2022-03-17 15:09:29 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,13 @@
+terraform {
+  backend "azure"{}
+}
 
 provider "azurerm" {
-    version = "2.20.0"
+    version = "=2.20.0"
  features {}
 }
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
-  tags = {
-    "owner" = "osman"
-  }
-}
-output "id" {
-    value = data.azurerm_resource_group.example.id
-  
+ 
 }
